f1b89e57435934e5a8b9eca9f9685d2658b8fd9d
Sonia <saguillen1@gmail.com>
2024-06-02 07:10:40 -0700

    initial setup

---
+++ config/site.ts
@@ -0,0 +1,27 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Alexander Noble",
+  description:
+    "I'm a Salesforce developer & app designer by trade. I'm also a father, adventurer, and music producer.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Projects",
+      href: "/projects",
+    },
+    {
+      title: "About me",
+      href: "/about",
+    },
+  ],
+  links: {
+    github: "https://github.com/anoble2020",
+    trailhead: "https://www.salesforce.com/trailblazer/anoble562",
+    linkedin: "https://www.linkedin.com/in/alexander-noble-75481b37/",
+    soundcloud: "https://soundcloud.com/"
+  },
+}

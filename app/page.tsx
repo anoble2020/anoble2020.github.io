44599d85d5c3bee9bc3687fd36ba4a0bde65db43
Alex Noble <alexander.noble@neuraflash.com>
2024-06-03 14:25:18 -0700

    more animations :)

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,30 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          hello 👋
+          hello there &nbsp;
+          <motion.span
+              animate={{ rotate: [0, 20, 0, -20, 0] }}
+              transition={{ duration: 1, repeat: Infinity, ease:"easeOut"}}
+              style={{ display: 'inline-block' }}
+            >
+              👋
+            </motion.span>
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-        I&apos;m a Salesforce and web app developer by trade. I&apos;m also a father, adventurer, and music producer. Give me a 
-        <a href={`mailto: ${siteConfig.email}`}> shout</a> if you want to chat.
+        I&apos;m a Salesforce and web app developer by trade. I&apos;m also a father, adventurer, and music producer. Give me a{' '} 
+        <motion.a href={`mailto: ${siteConfig.email}`}>
+        <motion.button
+          style={{
+            outline: 'none',
+            display: 'inline-block',
+            verticalAlign: 'middle'
+          }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >        
+          <b>shout</b>
+        </motion.button>
+        </motion.a>{' '}if you want to chat.
         </p>
       </div>
       <div className="flex gap-4">
